3a5b6b035c32d1bcd9aca024735a0f85e7b5dc82
Ahmad-Omar <aw.omar7@gmail.com>
2021-01-24 21:57:17 +0200

    active hamburger menu on mobile and tablet

--- src/components/Header/Navbar/index.js
+++ src/components/Header/Navbar/index.js
@@ -1,11 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import "./style.css";
 import Logo from "./Logo";
 import NavItems from "./NavItems";
 import PhoneNumber from "./PhoneNumber";
 import MenuRoundedIcon from "@material-ui/icons/MenuRounded";
+import CloseRoundedIcon from "@material-ui/icons/CloseRounded";
 
 function Navbar() {
+  const [menuToggle, setMenuToggle] = useState(false);
+  const handleClick = (e) => {
+    menuToggle === false ? setMenuToggle(true) : setMenuToggle(false);
+    // console.log(menuToggle);
+  };
+  const menuStyle = {
+    height: menuToggle === true ? "224px" : "0",
+  };
   return (
     <>
       <nav className="navbar hidden-mobile">
@@ -18,10 +27,24 @@ function Navbar() {
       <nav className="nav-mobile hidden-desktop">
         <div className="nav-mobile-wrapper">
           <Logo />
-          <div className="menu-icon">
-            <MenuRoundedIcon style={{ fontSize: "30", cursor: "pointer" }} />
+          <div
+            className="menu-icon"
+            style={{ fontSize: "30", cursor: "pointer" }}
+            onClick={handleClick}
+          >
+            {menuToggle ? (
+              <CloseRoundedIcon style={{ fontSize: "30px" }} />
+            ) : (
+              <MenuRoundedIcon style={{ fontSize: "30px" }} />
+            )}
           </div>
-          <div className="nav-menu-mobile hidden-desktop">
+          <div
+            className="nav-menu-mobile hidden-desktop"
+            style={menuStyle}
+            onClick={() => {
+              setMenuToggle(false);
+            }}
+          >
             <NavItems />
           </div>
         </div>
